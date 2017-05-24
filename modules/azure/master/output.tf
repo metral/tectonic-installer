a7a844372140e9d422101d8dacee598863ae060c
Mike Metral <metral@gmail.com>
2017-05-24 09:12:07 +0000

    output the expected DNS names for each master FQDN

--- modules/azure/master/output.tf
+++ modules/azure/master/output.tf
@@ -7,17 +7,17 @@ output "console_ip_address" {
 }
 
 output "ingress_external_fqdn" {
-  value = "${azurerm_lb.tectonic_lb.frontend_ip_configuration.1.private_ip_address}"
+  value = "${var.cluster_name}.${var.base_domain}"
 }
 
 output "ingress_internal_fqdn" {
-  value = "${azurerm_lb.tectonic_lb.frontend_ip_configuration.1.private_ip_address}"
+  value = "${var.cluster_name}.${var.base_domain}"
 }
 
 output "api_external_fqdn" {
-  value = "${azurerm_lb.proxy_lb.frontend_ip_configuration.0.private_ip_address}"
+  value = "${var.cluster_name}-k8s.${var.base_domain}"
 }
 
 output "api_internal_fqdn" {
-  value = "${azurerm_lb.proxy_lb.frontend_ip_configuration.0.private_ip_address}"
+  value = "${var.cluster_name}-k8s.${var.base_domain}"
 }
