d06f71cbf30bb94d4210c02849ce06d2cf0e0709
Mike Metral <metral@gmail.com>
2017-05-24 22:51:18 +0000

    add script to set the cluster DNS A records using nsupdate.
    Note, this assumes the user has an established route to the nameserver.

--- modules/azure/etcd/lb.tf
+++ modules/azure/etcd/lb.tf
@@ -35,3 +35,34 @@ resource "azurerm_lb_backend_address_pool" "etcd-lb" {
   resource_group_name = "${var.resource_group_name}"
   loadbalancer_id     = "${azurerm_lb.tectonic_etcd_lb.id}"
 }
+
+data "template_file" "scripts_nsupdate" {
+  template = <<EOF
+update delete $${cluster_name}-etcd.$${base_domain} A
+update add $${cluster_name}-etcd.$${base_domain} 0 A $${etcd_lb_ip_address}
+send
+EOF
+
+  vars {
+    cluster_name       = "${var.cluster_name}"
+    base_domain        = "${var.base_domain}"
+    etcd_lb_ip_address = "${azurerm_lb.tectonic_etcd_lb.frontend_ip_configuration.0.private_ip_address}"
+  }
+}
+
+resource "local_file" "nsupdate" {
+  content  = "${data.template_file.scripts_nsupdate.rendered}"
+  filename = "${path.cwd}/generated/dns/etcd-dns.txt"
+}
+
+resource "null_resource" "scripts_nsupdate" {
+  depends_on = ["local_file.nsupdate"]
+
+  triggers {
+      md5 = "${md5(data.template_file.scripts_nsupdate.rendered)}"
+  }
+
+  provisioner "local-exec" {
+    command = "nsupdate -d ${path.cwd}/generated/dns/etcd-dns.txt"
+  }
+}
