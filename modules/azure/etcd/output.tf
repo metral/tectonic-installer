8f6f7a7bb4d06e56481e74d4d990adb35b3ed4ca
Stephen Augustus <augustus@coreos.com>
2017-05-24 00:43:30 -0700

    azure/nsg: Initial commits for Azure NSG creation (#337)

--- modules/azure/etcd/output.tf
+++ modules/azure/etcd/output.tf
@@ -1,3 +1,8 @@
 output "ip_address" {
   value = ["${azurerm_lb.tectonic_etcd_lb.frontend_ip_configuration.0.private_ip_address}"]
 }
+
+#output "lb_ip" {
+#  value = "${azurerm_public_ip.etcd_publicip.ip_address}"
+#}
+
