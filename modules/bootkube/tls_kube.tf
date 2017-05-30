55f32db8042f38eb00d0e239228c125641f4f544
Duffie Cooley <duffie.cooley@coreos.com>
2017-05-29 20:35:15 -0700

    apicert: Add existing certificate support

--- modules/bootkube/tls_kube.tf
+++ modules/bootkube/tls_kube.tf
@@ -1,9 +1,9 @@
 # NOTE: Across this module, the following syntax is used at various places:
-#   `"${var.ca_cert == "" ? join(" ", tls_private_key.kube-ca.*.private_key_pem) : var.ca_key}"`
+#   `"${var.existing_certs["ca_crt_path"] == "/dev/null" ? join(" ", tls_private_key.kube_ca.*.private_key_pem) : var.existing_certs["ca_key"]}"`
 #
 # Due to https://github.com/hashicorp/hil/issues/50, both sides of conditions
 # are evaluated, until one of them is discarded. Unfortunately, the
-# `{tls_private_key/tls_self_signed_cert}.kube-ca` resources are created
+# `{tls_private_key/tls_self_signed_cert}.kube_ca` resources are created
 # conditionally and might not be present - in which case an error is
 # generated. Because a `count` is used on these ressources, the resources can be
 # referenced as lists with the `.*` notation, and arrays are allowed to be
@@ -50,13 +50,22 @@ resource "local_file" "kube_ca_crt" {
   filename = "./generated/tls/ca.crt"
 }
 
+#This cert will be used as --client-ca-file any request presenting a client certificate signed by one of the authorities in the client-ca-file is authenticated with an identity corresponding to the CommonName of the client certificate.
+
+resource "local_file" "kube_client_ca" {
+  content  = "${var.existing_certs["ca_key_path"] == "/dev/null" ? join(" ", tls_self_signed_cert.kube_ca.*.cert_pem) : file(var.existing_certs["ca_cert_path"])}"
+  filename = "./generated/tls/client-ca.crt"
+}
+
 # Kubernetes API Server (resources/generated/tls/{apiserver.key,apiserver.crt})
 resource "tls_private_key" "apiserver" {
+  count     = "${var.existing_certs["apiserver_key_path"] == "/dev/null" ? 1 : 0}"
   algorithm = "RSA"
   rsa_bits  = "2048"
 }
 
 resource "tls_cert_request" "apiserver" {
+  count           = "${var.existing_certs["apiserver_key_path"] == "/dev/null" ? 1 : 0}"
   key_algorithm   = "${tls_private_key.apiserver.algorithm}"
   private_key_pem = "${tls_private_key.apiserver.private_key_pem}"
 
@@ -79,6 +88,7 @@ resource "tls_cert_request" "apiserver" {
 }
 
 resource "tls_locally_signed_cert" "apiserver" {
+  count            = "${var.existing_certs["apiserver_key_path"] == "/dev/null" ? 1 : 0}"
   cert_request_pem = "${tls_cert_request.apiserver.cert_request_pem}"
 
   ca_key_algorithm   = "${var.ca_cert == "" ? join(" ", tls_self_signed_cert.kube_ca.*.key_algorithm) : var.ca_key_alg}"
@@ -121,7 +131,7 @@ resource "local_file" "service_account_crt" {
   filename = "./generated/tls/service-account.pub"
 }
 
-# Kubelet
+# Kubelet. This is really only used as client auth. The kubelets generate their own keys.
 resource "tls_private_key" "kubelet" {
   algorithm = "RSA"
   rsa_bits  = "2048"
