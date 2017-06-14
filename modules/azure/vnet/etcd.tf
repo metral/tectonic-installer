332f64f16825f12003954640c49a2b4044873ed4
Mike Metral <metral@gmail.com>
2017-06-14 19:52:05 +0000

    rebase with coreos/tectonic-installer master
    azure/etcd: remove etcd lb as done upstream
    
    etcd lb got moved into the vnet module and removed from the etcd module.
    This has been removed upstream, but was reintroduced during the rebase
    of the azure branch onto recent upstream/master.
    
    azure/nginx-proxy: mv api-proxy into master-as module
    
    Also, move the nsupdate script out of the master-as/lb.tf into
    master-as/api-proxy-lb.tf.
    
    azure/etcd: remove public IP from etcd
    
    azure/etcd: rm nsg from nic, since nsg's are applied to subnet
    
    azure/etcd: re-enable nsupdate after rebase was done/etcd moved to vnet
    
    azure/master: output private ip addresses of master AS

--- modules/azure/vnet/etcd.tf
+++ modules/azure/vnet/etcd.tf
@@ -1,9 +1,8 @@
 resource "azurerm_network_interface" "etcd_nic" {
-  count                     = "${var.etcd_count}"
-  name                      = "${var.tectonic_cluster_name}-etcd-nic-${count.index}"
-  location                  = "${var.location}"
-  network_security_group_id = "${azurerm_network_security_group.etcd_group.id}"
-  resource_group_name       = "${var.resource_group_name}"
+  count               = "${var.etcd_count}"
+  name                = "${var.tectonic_cluster_name}-etcd-nic-${count.index}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group_name}"
 
   ip_configuration {
     name                                    = "tectonic_etcd_configuration"
@@ -75,17 +74,9 @@ resource "azurerm_lb" "tectonic_etcd_lb" {
   frontend_ip_configuration {
     name = "default"
 
-    public_ip_address_id          = "${azurerm_public_ip.etcd_publicip.id}"
+    subnet_id                     = "${var.subnet}"
     private_ip_address_allocation = "dynamic"
   }
-}
-
-resource "azurerm_public_ip" "etcd_publicip" {
-  name                         = "${var.tectonic_cluster_name}_etcd_publicip"
-  location                     = "${var.location}"
-  resource_group_name          = "${var.resource_group_name}"
-  public_ip_address_allocation = "static"
-  domain_name_label            = "${var.tectonic_cluster_name}-etcd"
 }
 
 resource "azurerm_lb_rule" "etcd-lb" {
@@ -112,4 +103,36 @@ resource "azurerm_lb_backend_address_pool" "etcd-lb" {
   name                = "${var.tectonic_cluster_name}-etcd-lb-pool"
   resource_group_name = "${var.resource_group_name}"
   loadbalancer_id     = "${azurerm_lb.tectonic_etcd_lb.id}"
+}
+
+data "template_file" "scripts_nsupdate" {
+  template = <<EOF
+update delete $${cluster_name}-etcd.$${base_domain} A
+update add $${cluster_name}-etcd.$${base_domain} 0 A $${etcd_lb_ip_address}
+
+send
+EOF
+
+  vars {
+    cluster_name       = "${var.tectonic_cluster_name}"
+    base_domain        = "${var.base_domain}"
+    etcd_lb_ip_address = "${azurerm_lb.tectonic_etcd_lb.frontend_ip_configuration.0.private_ip_address}"
+  }
+}
+
+resource "local_file" "nsupdate" {
+  content  = "${data.template_file.scripts_nsupdate.rendered}"
+  filename = "${path.cwd}/generated/dns/etcd-dns.txt"
+}
+
+resource "null_resource" "scripts_nsupdate" {
+  depends_on = ["local_file.nsupdate"]
+
+  triggers {
+    md5 = "${md5(data.template_file.scripts_nsupdate.rendered)}"
+  }
+
+  provisioner "local-exec" {
+    command = "nsupdate -d ${path.cwd}/generated/dns/etcd-dns.txt"
+  }
 }
