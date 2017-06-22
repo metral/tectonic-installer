777d81f04625d4640491600fd2788917df4a8522
Mike Metral <metral@gmail.com>
2017-06-22 06:39:15 +0000

    switch etcd, master & workers to use CL 1353.8.0 instead of latest

--- modules/azure/master-as/master.tf
+++ modules/azure/master-as/master.tf
@@ -60,7 +60,7 @@ resource "azurerm_virtual_machine" "tectonic_master" {
     publisher = "CoreOS"
     offer     = "CoreOS"
     sku       = "Stable"
-    version   = "latest"
+    version   = "1353.8.0"
   }
 
   storage_os_disk {
