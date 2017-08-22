dfdd38d73f39b172a1cb3f1aeff722dc244601d4
Richard Lander <lander2k2@protonmail.com>
2017-08-22 11:27:06 -0400

    Add on/off switch for VMware outgoing HTTP proxy

--- platforms/vmware/main.tf
+++ platforms/vmware/main.tf
@@ -31,9 +31,10 @@ module "etcd" {
   vm_disk_template        = "${var.tectonic_vmware_vm_template}"
   vm_disk_template_folder = "${var.tectonic_vmware_vm_template_folder}"
   vmware_folder           = "${vsphere_folder.tectonic_vsphere_folder.path}"
-  http_proxy           =  "${var.tectonic_vmware_httpproxy}"
-  https_proxy           =  "${var.tectonic_vmware_httpsproxy}"
-  no_proxy           =  "${var.tectonic_vmware_noproxy}"
+  http_proxy_enabled      = "${var.tectonic_vmware_httpproxy_enabled}"
+  http_proxy              = "${var.tectonic_vmware_httpproxy}"
+  https_proxy             = "${var.tectonic_vmware_httpsproxy}"
+  no_proxy                = "${var.tectonic_vmware_noproxy}"
 
 }
 
@@ -71,9 +72,10 @@ module "masters" {
   kubeconfig              = "${module.bootkube.kubeconfig}"
   private_key             = "${var.tectonic_vmware_ssh_private_key_path}"
   image_re                = "${var.tectonic_image_re}"
-  http_proxy           =  "${var.tectonic_vmware_httpproxy}"
-  https_proxy           =  "${var.tectonic_vmware_httpsproxy}"
-  no_proxy           =  "${var.tectonic_vmware_noproxy}"
+  http_proxy_enabled      = "${var.tectonic_vmware_httpproxy_enabled}"
+  http_proxy              =  "${var.tectonic_vmware_httpproxy}"
+  https_proxy             =  "${var.tectonic_vmware_httpsproxy}"
+  no_proxy                =  "${var.tectonic_vmware_noproxy}"
 
 }
 
@@ -110,8 +112,9 @@ module "workers" {
   kubeconfig              = "${module.bootkube.kubeconfig}"
   private_key             = "${var.tectonic_vmware_ssh_private_key_path}"
   image_re                = "${var.tectonic_image_re}"
-  http_proxy           =  "${var.tectonic_vmware_httpproxy}"
-  https_proxy           =  "${var.tectonic_vmware_httpsproxy}"
-  no_proxy           =  "${var.tectonic_vmware_noproxy}"
+  http_proxy_enabled      = "${var.tectonic_vmware_httpproxy_enabled}"
+  http_proxy              =  "${var.tectonic_vmware_httpproxy}"
+  https_proxy             =  "${var.tectonic_vmware_httpsproxy}"
+  no_proxy                =  "${var.tectonic_vmware_noproxy}"
 
 }
