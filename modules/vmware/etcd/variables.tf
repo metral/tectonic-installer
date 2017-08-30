07fe2625bd8210eca52681f30361eb6bdd308ca2
Richard Lander <lander2k2@protonmail.com>
2017-08-29 20:41:31 -0400

    ca_cert: Add ability to add a custom CA to the trusted CAs

--- modules/vmware/etcd/variables.tf
+++ modules/vmware/etcd/variables.tf
@@ -142,3 +142,8 @@ variable "no_proxy" {
   type        = "string"
   description = "no_proxy variable for Nodes"
 }
+
+variable "trusted_ca" {
+  type        = "string"
+  description = "Path to CA to add to trusted CAs on cluster nodes"
+}
