6a63e291f483caa3bfb7c65d0761743f09fab4c5
Richard Lander <lander2k2@protonmail.com>
2017-08-30 23:29:43 -0400

    multiaz_datacenter: Fix shit datacenter variable

--- modules/vmware/node/nodes.tf
+++ modules/vmware/node/nodes.tf
@@ -1,7 +1,7 @@
 resource "vsphere_virtual_machine" "node" {
   count      = "${var.instance_count}"
   name       = "${var.hostname["${count.index}"]}"
-  datacenter = "${var.vmware_datacenter["${count.index}"]}"
+  datacenter = "${var.vmware_datacenters["${count.index}"]}"
   cluster    = "${var.vmware_cluster}"
   vcpu       = "${var.vm_vcpu}"
   memory     = "${var.vm_memory}"
