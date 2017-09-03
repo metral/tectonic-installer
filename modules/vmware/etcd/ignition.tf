f6530a0fb4c85b673701d2e428f6e0c5e2d7d04e
Richard Lander <rlander7@ford.com>
2017-09-03 19:12:49 -0400

    Properly set NTP source values

--- modules/vmware/etcd/ignition.tf
+++ modules/vmware/etcd/ignition.tf
@@ -276,7 +276,7 @@ EOF
 }
 
 data "ignition_file" "ntp_conf" {
-  count      = "${var.ntp_sources ? 1 : 0}"
+  count      = "${length(keys(var.ntp_sources)) > 0 ? 1 : 0}"
   path       = "/etc/systemd/timesyncd.conf"
   mode       = 0644
   filesystem = "root"
@@ -284,7 +284,7 @@ data "ignition_file" "ntp_conf" {
   content {
     content = <<EOF
 [Time]
-NTP=19.88.151.249 19.88.151.247
+NTP=${var.ntp_sources["${count.index}"]}
 EOF
   }
 }
