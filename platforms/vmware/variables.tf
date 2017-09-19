c86bb1127a4bd705fd0b8e812be9cdd051185dc1
Richard Lander <rlander7@ford.com>
2017-09-19 10:17:15 -0400

    resource pool code fix

--- platforms/vmware/variables.tf
+++ platforms/vmware/variables.tf
@@ -119,7 +119,7 @@ variable "tectonic_vmware_etcd_resource_pool" {
 
   description = <<EOF
   terraform map of etcd node(s) vSphere Resource Pools, example:
-  tectonic_vmware_etcd_clusters = {
+  tectonic_vmware_etcd_resource_pool = {
   "0" = "myresourcepool-1"
   "1" = "myresourcepool-2"
   "2" = "myresourcepool-3"
@@ -249,7 +249,7 @@ variable "tectonic_vmware_master_resource_pool" {
 
   description = <<EOF
   terraform map of master node(s) vSphere Resource pools, example:
-  tectonic_vmware_master_clusters = {
+  tectonic_vmware_master_resource_pool = {
   "0" = "myresourcepool-0"
   "1" = "myresourcepool-1"
   "2" = "myresourcepool-2"
@@ -374,7 +374,7 @@ variable "tectonic_vmware_worker_resource_pool" {
 
   description = <<EOF
   terraform map of worker node(s) vSphere Resource Pools, example:
-  tectonic_vmware_worker_clusters = {
+  tectonic_vmware_worker_resource_pool = {
   "0" = "myresourcepool-0"
   "1" = "myresourcepool-1"
 }
